29f672a27068b965851b2a8b1b8bb6fd1142f9a3
Евгений Мишарев <misharevea@gmail.com>
2022-03-28 09:16:12 +0300

    Добавьте файлы проекта.

---
+++ RoomFinishNumerator/RoomFinishNumeratorCommand.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomFinishNumerator
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    class RoomFinishNumeratorCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            return Result.Succeeded;
+        }
+    }
+}
